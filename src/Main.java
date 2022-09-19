397e28ec41fff2c93b1ef10090969adfc0f42dc4
jah <celt8@yandex.ru>
2022-09-19 18:31:06 +0500

    - jehm v2

--- src/Main.java
+++ src/Main.java
@@ -2,35 +2,42 @@ import java.util.regex.Pattern;
 
 public class Main {
     private static final Pattern pattern = Pattern.compile("[a-zA-Z0-9_]*");
+
     public static void main(String[] args) {
+
+        System.out.println(checkEnter("login", "password", "pass"));
+
+    }
+
+    public static Boolean checkEnter(String login, String password, String confirmPassowrd) {
+        Boolean check = true;
         try {
-            checkEnter("xcvb", "sdf", "f");
+            if (login == null || login.isEmpty() || login.length() > 20) {
+                throw new WrongLoginException("Не допустивая длина логина");
+            } else if (!login.matches(pattern.pattern())) {
+                throw new WrongLoginException("Логин содержит не корректные символы");
+            }
+            if (password == null || password.isEmpty() || password.length() >= 20) {
+                throw new WrongLoginException("Не допустивая длина пароля");
+            } else if (!password.matches(pattern.pattern())) {
+                throw new WrongPasswordException("Пароль содержит не корректные символы");
+            }
+            if (confirmPassowrd == null || confirmPassowrd.isEmpty() || confirmPassowrd.length() >= 20) {
+                throw new WrongLoginException("Не допустивая длина подтверждения пароля");
+            } else if (!password.matches(pattern.pattern())) {
+                throw new WrongPasswordException("Подтверждение пароля содержит не корректные символы");
+            }
+            if (!password.equals(confirmPassowrd)) {
+                throw new WrongPasswordException("Пароли не одинаковые");
+            }
         } catch (WrongLoginException e) {
+            check = false;
             System.out.println(e.getMessage());
         } catch (WrongPasswordException e) {
+            check = false;
             System.out.println(e.getMessage());
-        }finally {
-            System.out.println("Misson imposible complete");
-        }
-    }
-    public static void checkEnter(String login, String password, String confirmPassowrd){
-        if(login ==null || login.isEmpty() || login.length() > 20){
-            throw new WrongLoginException("Не допустивая длина логина");
-        } else if (!login.matches(pattern.pattern())) {
-            throw new WrongLoginException("Логин содержит не корректные символы");
-        }
-        if(password ==null || password.isEmpty() || password.length() >= 20){
-            throw new WrongLoginException("Не допустивая длина пароля");
-        }else if (!password.matches(pattern.pattern())) {
-            throw new WrongPasswordException("Пароль содержит не корректные символы");
-        }
-        if(confirmPassowrd ==null || confirmPassowrd.isEmpty() || confirmPassowrd.length() >= 20){
-            throw new WrongLoginException("Не допустивая длина подтверждения пароля");
-        }else if (!password.matches(pattern.pattern())) {
-            throw new WrongPasswordException("Подтверждение пароля содержит не корректные символы");
-        }
-        if(!password.equals(confirmPassowrd)){
-            throw new WrongPasswordException("Пароли не одинаковые");
+        } finally {
+            return check;
         }
     }
 }
